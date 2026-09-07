f0d1349a91b663a9143e0bc3761c43a63807ee5f
Brett Lange <brett.lange@colorado.edu>
2026-09-07 08:15:13 -0500

    updated paths

--- run-qemu.sh
+++ run-qemu.sh
@@ -4,9 +4,9 @@
 qemu-system-aarch64 \
     -M virt  \
     -cpu cortex-a53 -nographic -smp 1 \
-    -kernel ./qemu-build/Image \
+    -kernel ./buildroot/output/images/Image \
     -append "rootwait root=/dev/vda console=ttyAMA0" \
     -netdev user,id=eth0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000 \
     -device virtio-net-device,netdev=eth0 \
-    -drive file=./qemu-build/rootfs.ext4,if=none,format=raw,id=hd0 \
+    -drive file=./buildroot/output/images/rootfs.ext4,if=none,format=raw,id=hd0 \
     -device virtio-blk-device,drive=hd0 -device virtio-rng-pci
