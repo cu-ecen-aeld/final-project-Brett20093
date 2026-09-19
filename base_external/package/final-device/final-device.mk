326dac6a1c77ece306fd26bf11ef544d50924bdb
Brett Lange <brett.lange@colorado.edu>
2026-09-19 14:22:09 -0500

    updated network and start app on boot

--- base_external/package/final-device/final-device.mk
+++ base_external/package/final-device/final-device.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 
-FINAL_DEVICE_VERSION = 714d580c5a9b4532b9ead910b0035f045044d0e4
+FINAL_DEVICE_VERSION = 5581f9bd53f1f606f79dfef8f73a7350e8356fb6
 FINAL_DEVICE_SITE = git@github.com:Brett20093/ECEA-5307-Final-Project-Devices.git
 FINAL_DEVICE_SITE_METHOD = git
 FINAL_DEVICE_GIT_SUBMODULES = YES
@@ -14,7 +14,7 @@ FINAL_DEVICE_MODULE_SUBDIRS = reed-switch freezer-monitor lcd
 define FINAL_DEVICE_BUILD_CMDS
 	$(MAKE) -C $(LINUX_DIR) M=$(@D)/reed-switch $(LINUX_MAKE_FLAGS) modules
 	$(MAKE) -C $(LINUX_DIR) M=$(@D)/lcd $(LINUX_MAKE_FLAGS) modules
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) CFLAGS="-O0 -g -Wall -Werror -I$(@D)/lcd" -C $(@D)/freezer-monitor all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) CFLAGS="-O0 -g -Wall -Werror -Wno-format-truncation -I$(@D)/lcd" -C $(@D)/freezer-monitor all
 endef
 
 # Install
@@ -23,6 +23,7 @@ define FINAL_DEVICE_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/reed-switch/*.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra
 	$(INSTALL) -m 0755 $(@D)/lcd/*.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra
 	$(INSTALL) -m 0755 $(@D)/freezer-monitor/freezer_monitor $(TARGET_DIR)/usr/bin
+    $(INSTALL) -m 0755 $(@D)/freezer-monitor/freezermonitor.service $(TARGET_DIR)/etc/systemd/system/freezermonitor.service
 endef
 
 $(eval $(kernel-module))
